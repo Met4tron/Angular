7a8fd4f565e346545b531773ff10d2e7673022a9
Yuri Gomes <desenvolvedoryuri@gmail.com>
2016-11-27 22:32:05 -0200

    Curso Javascript Alura - Exercicios Práticos

---
+++ Introducao-Alura/adiciona.js
@@ -0,0 +1,24 @@
+var botao = document.querySelector('#adicionar-paciente');
+
+botao.addEventListener("click",function(event){
+  event.preventDefault(); // Impede comportamento Padrão
+
+  var campoNome = document.querySelector('#campo-nome');
+  var campoPeso = document.querySelector('#campo-peso');
+  var campoAltura = document.querySelector('#campo-altura');
+
+
+
+  var pacienteNovo = "<tr class = 'paciente'>" +
+      "<td class='info-nome'>"+ campoNome.value +"</td>" +
+      "<td class='info-peso'>"+ campoPeso.value +"</td>" +
+      "<td class='info-altura'>"+ campoAltura.value +"</td>" +
+      "<td class='info-imc'>"+ campoPeso.value / (campoAltura.value * campoAltura.value) +"</td>" +
+      "</tr>";
+var tabela = document.querySelector('table');
+tabela.innerHTML = tabela.innerHTML + pacienteNovo;
+
+campoNome.value="";
+campoAltura.value="";
+campoPeso.value="";
+});
