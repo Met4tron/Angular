70537046e104c88673b004bc014bc7b7f93e3192
Yuri Gomes <desenvolvedoryuri@gmail.com>
2016-12-07 13:04:57 -0200

    ES6 - II

---
+++ aluraframe/client/js/app/services/ProxyFactory.js
@@ -0,0 +1,20 @@
+class ProxyFactory{
+
+  static create(objeto, props, acao){
+    return new Proxy(objeto, {
+      get(target, prop, receiver){
+        if(props.includes(prop) && typeof(target[prop])){
+          return function() {
+            Reflect.apply(target[prop], target, arguments);
+            return acao(target);
+          }
+        }
+        return Reflect.get(target, prop, receiver);
+      }
+    })
+  }
+
+  static _ehFuncao(func) {
+    return typeof(func) == typeof(Function);
+  }
+}
